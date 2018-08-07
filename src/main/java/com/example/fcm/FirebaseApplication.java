784a9d096ae46a7226b67c900ec27778c0ccdb84
iaroslav.soltys <iaroslav.soltys@globallogic.com>
2018-08-07 16:41:06 +0300

    init commit

---
+++ src/main/java/com/example/fcm/FirebaseApplication.java
@@ -0,0 +1,12 @@
+package com.example.fcm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirebaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirebaseApplication.class, args);
+	}
+}
---
+++ src/main/java/com/example/fcm/config/FirebaseConfiguration.java
@@ -0,0 +1,37 @@
+package com.example.fcm.config;
+
+import com.google.auth.oauth2.GoogleCredentials;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.FirebaseOptions;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+
+import javax.annotation.PostConstruct;
+import java.io.FileInputStream;
+import java.io.IOException;
+
+@Configuration
+public class FirebaseConfiguration {
+
+    @Value("${firebase.file-path}")
+    private String filePath;
+
+    @Value("${firebase.database-url}")
+    private String databaseUrl;
+
+    @PostConstruct
+    public void init() {
+        try {
+            FirebaseOptions options = new FirebaseOptions.Builder()
+                    .setCredentials(GoogleCredentials.fromStream(new FileInputStream(filePath)))
+                    .setDatabaseUrl(databaseUrl)
+                    .build();
+            FirebaseApp.initializeApp(options);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
+    }
+
+}
