98cc1910d8871fde47ee31ee4fa7daaf1706bc55
iaroslav.soltys <iaroslav.soltys@globallogic.com>
2018-08-08 17:22:04 +0300

    created Notification service with implementation

---
+++ src/main/java/com/example/fcm/service/NotificationService.java
@@ -0,0 +1,16 @@
+package com.example.fcm.service;
+
+import com.google.firebase.messaging.FirebaseMessagingException;
+import com.google.firebase.messaging.Message;
+
+import java.util.List;
+
+public interface NotificationService {
+
+    void subscribeToTopic(List<String> tokens, String topicName) throws FirebaseMessagingException;
+
+    void unsubscribeFromTopic(List<String> tokens, String topicName) throws FirebaseMessagingException;
+
+    String sendMessage(Message message) throws FirebaseMessagingException;
+
+}
---
+++ src/main/java/com/example/fcm/service/impl/NotificationServiceImpl.java
@@ -0,0 +1,37 @@
+package com.example.fcm.service.impl;
+
+import com.example.fcm.service.NotificationService;
+import com.google.firebase.messaging.FirebaseMessagingException;
+import com.google.firebase.messaging.Message;
+import com.google.firebase.messaging.TopicManagementResponse;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+import static com.google.firebase.messaging.FirebaseMessaging.getInstance;
+
+@Slf4j
+@Service
+public class NotificationServiceImpl implements NotificationService {
+
+    @Override
+    public void subscribeToTopic(List<String> tokens, String topicName) throws FirebaseMessagingException {
+        TopicManagementResponse response = getInstance().subscribeToTopic(tokens, topicName);
+        log.info("{} device successfully subscribed on topic '{}'", response.getSuccessCount(), topicName);
+    }
+
+    @Override
+    public void unsubscribeFromTopic(List<String> tokens, String topicName) throws FirebaseMessagingException {
+        TopicManagementResponse response = getInstance().unsubscribeFromTopic(tokens, topicName);
+        log.info("{} device successfully unsubscribed from topic '{}'", response.getSuccessCount(), topicName);
+    }
+
+    @Override
+    public String sendMessage(Message message) throws FirebaseMessagingException {
+        String messageId = getInstance().send(message);
+        log.info("Message send successfully. message id: '{}'", messageId);
+        return messageId;
+    }
+
+}
