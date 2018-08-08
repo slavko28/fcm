194573cdea3bdc08a37402cc6dd1a1d8a91bcc28
iaroslav.soltys <iaroslav.soltys@globallogic.com>
2018-08-08 14:15:51 +0300

    added field to Device

--- src/main/java/com/example/fcm/model/Device.java
+++ src/main/java/com/example/fcm/model/Device.java
@@ -14,13 +14,12 @@ public class Device {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-
     private Long externalId;
-
     @Enumerated(EnumType.STRING)
     private DevicePlatform platform;
     @ManyToOne
     private Topic topics;
     private String token;
+    private Boolean enable;
 
 }
