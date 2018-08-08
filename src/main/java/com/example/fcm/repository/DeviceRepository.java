36b4d751e5bed91dc17811288f222d7636fb490f
iaroslav.soltys <iaroslav.soltys@globallogic.com>
2018-08-08 14:16:24 +0300

    added methods

--- src/main/java/com/example/fcm/repository/DeviceRepository.java
+++ src/main/java/com/example/fcm/repository/DeviceRepository.java
@@ -4,6 +4,11 @@ import com.example.fcm.model.Device;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface DeviceRepository extends CrudRepository<Device, Long>{
+    Optional<Device> findByExternalId(Long externalId);
+
+    void deleteByExternalId(Long externalId);
 }
--- src/main/java/com/example/fcm/repository/TopicRepository.java
+++ src/main/java/com/example/fcm/repository/TopicRepository.java
@@ -4,6 +4,11 @@ import com.example.fcm.model.Topic;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface TopicRepository extends CrudRepository<Topic, Integer>{
+    Optional<Topic> findByName(String name);
+
+    void deleteByName(String topicName);
 }
