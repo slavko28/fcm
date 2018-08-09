41bab6e35f744258aa313ac52e5d42d243837620
iaroslav.soltys <iaroslav.soltys@globallogic.com>
2018-08-09 15:33:45 +0300

    added DTO MessageRequest

---
+++ src/main/java/com/example/fcm/service/dto/MessageRequest.java
@@ -0,0 +1,17 @@
+package com.example.fcm.service.dto;
+
+import com.example.fcm.model.enums.NotificationType;
+import lombok.Data;
+import lombok.experimental.Builder;
+
+@Data
+@Builder
+public class MessageRequest {
+
+    private Long deviceId;
+    private Long externalId;
+    private String userLogin;
+    private String topicName;
+    private String messageBody;
+    private NotificationType notificationType;
+}
