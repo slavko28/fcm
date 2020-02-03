9cec5f628e91265305920a46629f56f9e781afea
yaroslav <slavkosoltys@ukr.net>
2020-02-03 13:48:41 +0200

    add logs on fail/success sending message

--- src/main/java/com/example/fcm/service/impl/ProducerServiceImpl.java
+++ src/main/java/com/example/fcm/service/impl/ProducerServiceImpl.java
@@ -6,9 +6,12 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.kafka.support.KafkaHeaders;
+import org.springframework.kafka.support.SendResult;
 import org.springframework.messaging.Message;
 import org.springframework.messaging.support.MessageBuilder;
 import org.springframework.stereotype.Service;
+import org.springframework.util.concurrent.ListenableFuture;
+import org.springframework.util.concurrent.ListenableFutureCallback;
 
 @Slf4j
 @Service
@@ -23,14 +26,29 @@ public class ProducerServiceImpl implements ProducerService {
 
     @Override
     public void publish(String topicName, MessageRequest request) {
+        Message<MessageRequest> message = getMessage(topicName, request);
+        sentMessage(request, message);
+    }
 
-        log.info("Sending message:\n request='{}'\n to topic='{}'", request, topicName);
+    public void sentMessage(MessageRequest request, Message<MessageRequest> message) {
+        ListenableFuture<SendResult<String, MessageRequest>> resultListenableFuture = kafkaTemplate.send(message);
+        resultListenableFuture.addCallback(new ListenableFutureCallback<SendResult<String, MessageRequest>>() {
+            @Override
+            public void onSuccess(SendResult<String, MessageRequest> result) {
+                log.info("Sent request=[{}] with offset=[{}]"+ request.toString(), result.getRecordMetadata().offset());
+            }
 
-        Message<MessageRequest> message = MessageBuilder
+            @Override
+            public void onFailure(Throwable throwable) {
+                log.error("Unable to send message=[{}] due to : {}", request.toString(), throwable.getMessage());
+            }
+        });
+    }
+
+    public Message<MessageRequest> getMessage(String topicName, MessageRequest request) {
+        return MessageBuilder
                 .withPayload(request)
                 .setHeader(KafkaHeaders.TOPIC, topicName)
                 .build();
-
-        kafkaTemplate.send(message);
     }
 }
